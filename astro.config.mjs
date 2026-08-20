6fedac6fe0c1469d8e80b3d6a40933f942204fac
ibeeng <bellamujia30@gmail.com>
2026-08-20 09:56:59 +0700

    resolve conflik merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-<<<<<<< HEAD
-
-// https://astro.build/config
-export default defineConfig({});
-=======
-import tailwind from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwind()],
+    plugins: [tailwindcss()],
   },
 });
->>>>>>> master
